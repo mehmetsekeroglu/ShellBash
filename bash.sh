84c339cd4add8944cbb25414e4bbf6144c4ace6f
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2021-02-22 15:20:40 +0100

    first commit

---
+++ bash.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/bash
+
+#echo Merhaba!
+
+#Creat a file an folder
+# mkdir yeniKlasör
+# touch "yeniKlasör/dosya.txt"
+# echo "Dosya ve klasör tek satirda olusturuldu">> "yeniKlasör/dosya.txt"
+# echo "islem tamamlandi!"
+
+#variables
+
+# NAME="Mehmet"
+# echo $NAME
+# echo ${NAME}
+
+#Get user input
+
+# read -p "enter your name:" NAME
+# echo $NAME
+
+#Conditionals
+# CITY="Cenevre"
+#  if [ "$CITY" == "Luzern" ]
+#  then
+#  echo "Luzern dogru tahmin"
+#  elif [ "$CITY" == "Zurih" ]
+#  then
+#  echo "Zurih dogru tahmin"
+#  else
+#  echo "Sehir dogru tehmin edilmedi"
+#  fi
+
+ #functions
+#  function yeniYiliYazdir() {
+#      echo "2021"
+#  }
+# yeniYiliYazdir
+
+# nelerOgreniyoruz (){
+#     echo "$1 ve $2 ogrendik, $3 ogreniyoruz"
+# }
+
+# nelerOgreniyoruz "HTML" "CSS" "JS"
+
+# printSomething(){
+#     echo Merhaba $1
+#     return 0
+# }
+
+# printSomething 2021
+# echo Function has return value $?
